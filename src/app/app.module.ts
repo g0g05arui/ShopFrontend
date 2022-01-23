c8a67389021a3befb9f8cad8443e79c9c29f3566
Mihai Indreias <mihai.indreias@gmail.com>
2022-01-23 16:08:13 +0200

    create user modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,29 @@ import { ProductsMainPageComponent } from './components/products-main-page/produ
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SignupModalComponent } from './modals/signup-modal/signup-modal.component';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {  MatInputModule } from '@angular/material/input';
+import {MatProgressSpinnerModule, MatSpinner} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsMainPageComponent,
-    SignupModalComponent
+    SignupModalComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modals/signup-modal/signup-modal.component.ts
+++ src/app/modals/signup-modal/signup-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 
@@ -9,8 +10,34 @@ import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./signup-modal.component.scss']
 })
 export class SignupModalComponent {
+   email = new FormControl('', [Validators.required, Validators.email]);
+   passwordConfirmed=new FormControl('', [Validators.required]);
+   password=new FormControl('', [Validators.required])
+   options: FormGroup;
+   isLoading:boolean=false;
+   getErrorMessage():string {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+   constructor(@Inject(MAT_DIALOG_DATA) public data:any,public formBuilder:FormBuilder) {
+     this.passwordConfirmed.addValidators((value:AbstractControl):ValidationErrors=>{
+        if(this.passwordConfirmed.value!=this.password.value)
+          return {"error":"Emails do not match"};
+        return {};
+     });
 
-   constructor(@Inject(MAT_DIALOG_DATA) public data:any) {}
-  
+     this.options = this.formBuilder.group({
+      email: this.email,
+      passwordConfirmed: this.passwordConfirmed,
+      password:this.password
+    });
 
+   }
+   onSubmit():void{
+      if(this.options.valid){
+          this.isLoading=true;
+      }
+  }
 }
